da886a1874524defc780a734a3da2f29e2bec787
Paul Jokotagba <35919394+kingjokes@users.noreply.github.com>
2024-04-21 17:12:15 +0100

    quick fix

--- src/main.ts
+++ src/main.ts
@@ -3,11 +3,10 @@ import { AppModule } from "./app.module";
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-
+ 
   app.enableCors({
-    origin: [
-      ...process.env.ALLOWED_URL?.split(' ')
-    ],
+    origin: process.env.ALLOWED_URL?.split(' ')
+    ,
     methods: ["GET", "POST", "DELETE", "PUT", "PATCH"],
     credentials: true,
   });
